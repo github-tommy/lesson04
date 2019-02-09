a03d2a06c42f29dae0a71a43dd8766d4bae16926
tom <tommy.surjadi@gmail.com>
2019-02-08 17:24:00 -0800

    Created money creation calc lesson04

---
+++ squareCalc.js
@@ -0,0 +1,9 @@
+let name = prompt('enter your name');
+let initialInvestment = prompt ('enter your investment amount');
+let estimatedInvestmentLater;
+
+estimatedInvestmentLater = initialInvestment ** 2;
+
+alert('Congratulation '+ name + ', your estimated investment value is US$'+estimatedInvestmentLater + ' in 10 years - subject to market risks. Thank you.');
+
+// input# and square it.
