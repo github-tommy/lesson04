881f46572cff14a0b95fd5158f8ae26c69c4a073
tom <tommy.surjadi@gmail.com>
2019-02-08 18:53:00 -0800

    Worked on the click button and add another calc

---
+++ justForFunRetirementCalc.js
@@ -0,0 +1,18 @@
+let name = prompt('enter your name');
+let expenses = prompt('enter your annual expenses in thousand dollars');
+let retirementAmount;
+
+retirementAmount = expenses * 30;
+
+alert('You will need about USD '+ retirementAmount + ' (000) to retire')
+
+
+
+
+// input# and double it. assuming rate of return of 100%.
+
+//let name = prompt('enter your name');
+//let investmentAmountNow = prompt ('enter your initial investment amount in thousand dollars');
+//let estimatedInvestmentValueLater= (investmentAmountNow * 2);
+
+//alert('Hi ' +name + ', Your estimated investment value is ' + estimatedInvestmentValueLater + '. Thank you for stopping-by.);
