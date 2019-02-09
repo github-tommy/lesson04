e419d687baec53ec5faa94f28915de8e2ee276b1
tom <tommy.surjadi@gmail.com>
2019-02-08 18:08:09 -0800

    Added 2nd number from user

--- squareCalc.js
+++ squareCalc.js
@@ -1,8 +1,9 @@
 let name = prompt('enter your name');
 let initialInvestment = prompt ('enter your investment amount');
+let subsequentInvestment = prompt ('enter number of times you plan to put in the plan');
 let estimatedInvestmentLater;
 
-estimatedInvestmentLater = initialInvestment ** 2;
+estimatedInvestmentLater = (initialInvestment * subsequentInvestment) ** 2;
 
 alert('Congratulation '+ name + ', your estimated investment value is US$'+estimatedInvestmentLater + ' in 10 years - subject to market risks. Thank you.');
 
