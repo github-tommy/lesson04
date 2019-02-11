acc96f44d07d1304998d65af7b7c575ec163df84
tom <tommy.surjadi@gmail.com>
2019-02-10 22:47:32 -0800

    Revised to include both calculation by clicking the boxes

--- squareCalc.js
+++ squareCalc.js
@@ -1,3 +1,4 @@
+function myFunction() {
 let name = prompt('enter your name');
 let initialInvestment = prompt ('enter your investment amount');
 let subsequentInvestment = prompt ('enter number of times you plan to put in the plan');
@@ -6,9 +7,16 @@ let estimatedInvestmentLater;
 estimatedInvestmentLater = (initialInvestment * subsequentInvestment) ** 2;
 
 alert('Congratulation '+ name + ', your estimated investment value is US$'+estimatedInvestmentLater + ' in 10 years - subject to market risks. Thank you.');
-
+}
 // input# and square it.
 
-function myFunction() {
-    document.getElementById('demo').innerHTML = 'Refresh to do new calculation. Thank you.';
+function myFunctionTwo() {
+let name = prompt('enter your name');
+let expenses = prompt('enter your annual expenses in thousand dollars');
+let retirementAmount;
+
+retirementAmount = expenses * 30;
+
+alert('You will need about USD '+ retirementAmount + ' (000) to retire')
 }
+// input# and double it. assuming rate of return of 100%.
